b01448fb054330cb4e10a4134c824d54569f451e
paloma anjos <105317967+PalomadAnjos@users.noreply.github.com>
2023-10-20 10:51:19 -0300

    OK

--- main.js
+++ main.js
@@ -1,3 +1,9 @@
+//Declaração/criação a função tocaSomPom
 function tocaSomPom() {
+    document.querySelector('#som_tecla_pom').play(); 
+ }
+ //chamada ou inovação da função tocaSomPom ao clicar no botão
+ document.querySelector('.tecla_pom').onclik = toca
+      
     document.querySelector('.#som_tecla_pom').play(); 
 }
