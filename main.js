606e8564484d0379d27e4e49de61635669ebc6e2
paloma anjos <105317967+PalomadAnjos@users.noreply.github.com>
2023-10-20 11:26:26 -0300

    jhh

--- main.js
+++ main.js
@@ -1,9 +1,58 @@
-//Declaração/criação a função tocaSomPom
-function tocaSomPom() {
-    document.querySelector('#som_tecla_pom').play(); 
+//Declaração/criação a função tocaSomPix
+function tocaSomPix() {
+    document.querySelector('#som_tecla_pix').play(); 
  }
- //chamada ou inovação da função tocaSomPom ao clicar no botão
- document.querySelector('.tecla_pom').onclik = toca
-      
-    document.querySelector('.#som_tecla_pom').play(); 
+ //chamada ou invocação da função tocaSomPix ao clicar no botão
+ document.querySelector('.tecla_pix').onclik = tocaSomPix;
+
+ //Declaração/criacão a função tocaSomRojão
+ function tocaSomRojão() {
+    document.querySelector('.tecla_rojão').onclick = tocaSomRojão;
+ }
+ //chamada ou invocação da função tocaSomRojão ao clicar no botão
+ document.querySelector('.tecla_rojão').onclick = tocaSomRojão;
+
+ //Declaração/criação a funcão tocaSomIndo
+ function tocaSomIndo() {
+    document.querySelector('#som_tecla_indo').play();
+ }
+ //chamada ou invocação da função  tocaSomIndo ao clicar no botão
+ document.querySelector('.tecla_indo').play(); {
 }
+//Declaração/criação a função tocaSomAaaa
+function tocaSomAaaa() {
+    document.querySelector('#om_tecla_aaaa').play();
+}
+ //chamada ou invocação da função  tocaSomAaa ao clicar no botão
+ document.querySelector('.tecla_aaaa').play(); {     
+}
+//Declaração/criação a funcão tocaSomSigma
+function tocaSomSigma() {
+    document.querySelector('#som_tecla_sigma').play();
+ }
+ //chamada ou invocação da função  tocaSomSigma ao clicar no botão
+ document.querySelector('.tecla_sigma').play(); {
+ }
+ //Declaração/criação a funcão tocaSomAcessa
+ function tocaSomAcessa() {
+    document.querySelector('#som_tecla_acessa').play();
+ }
+ //chamada ou invocação da função  tocaSomAcessa no clicar no botão
+ document.querySelector('.tecla_acessa').play(); {
+}
+//Declaração/criação a funcão tocaSomUepaa
+function tocaSomUepaa() {
+    document.querySelector('#som_tecla_uepaa').play();
+ }
+ //chamada ou invocação da função  tocaSomUepaa ao clicar no botão
+ document.querySelector('.tecla_uepaa').play(); {
+ }
+ //Declaração/criação a funcão tocaSomCade
+ function tocaSomCade() {
+    document.querySelector('#som_tecla_cade').play();
+ }
+ //chamada ou invocação da função  tocaSomCade ao clicar no botão
+ document.querySelector('.tecla_cade').play(); {
+}
+
+ 
