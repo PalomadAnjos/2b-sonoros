6f6c4247aaae14d12961b445ab24f4a5f22ce063
paloma anjos <105317967+PalomadAnjos@users.noreply.github.com>
2023-10-27 10:31:25 -0300

    ok

--- main.js
+++ main.js
@@ -2,63 +2,14 @@
 function tocaSomPix() {
     document.querySelector('#som_tecla_pix').play(); 
  }
- //chamada ou invocação da função tocaSomPix ao clicar no botão
- document.querySelector('.tecla_pix').onclik = tocaSomPix;
 
- //Declaração/criacão a função tocaSomRojão
- function tocaSomRojão() {
-    document.querySelector('.tecla_rojão').onclick = tocaSomRojão;
- }
- //chamada ou invocação da função tocaSomRojão ao clicar no botão
- document.querySelector('.tecla_rojão').onclick = tocaSomRojão;
-
- //Declaração/criação a funcão tocaSomIndo
- function tocaSomIndo() {
-    document.querySelector('#som_tecla_indo').play();
- }
- //chamada ou invocação da função  tocaSomIndo ao clicar no botão
- document.querySelector('.tecla_indo').play(); {
-}
-//Declaração/criação a função tocaSomAaaa
-function tocaSomAaaa() {
-    document.querySelector('#om_tecla_aaaa').play();
-}
- //chamada ou invocação da função  tocaSomAaa ao clicar no botão
- document.querySelector('.tecla_aaaa').play(); {     
-}
-//Declaração/criação a funcão tocaSomSigma
-function tocaSomSigma() {
-    document.querySelector('#som_tecla_sigma').play();
- }
- //chamada ou invocação da função  tocaSomSigma ao clicar no botão
- document.querySelector('.tecla_sigma').play(); {
- }
- //Declaração/criação a funcão tocaSomAcessa
- function tocaSomAcessa() {
-    document.querySelector('#som_tecla_acessa').play();
- }
- //chamada ou invocação da função  tocaSomAcessa no clicar no botão
- document.querySelector('.tecla_acessa').play(); {
-}
-//Declaração/criação a funcão tocaSomUepaa
-function tocaSomUepaa() {
-    document.querySelector('#som_tecla_uepaa').play();
- }
- //chamada ou invocação da função  tocaSomUepaa ao clicar no botão
- document.querySelector('.tecla_uepaa').play(); {
- }
- //Declaração/criação a funcão tocaSomCade
- function tocaSomCade() {
-    document.querySelector('#som_tecla_cade').play();
- }
- //chamada ou invocação da função  tocaSomCade ao clicar no botão
- document.querySelector('.tecla_cade').play(); {
-}
-//Declaração/criação a funcão tocaSomKiko
-function tocaSomkiko() {
-   document.querySelector('#som_tecla_kiko').play();
-}
-//chamada ou invocação da função  tocaSomCade ao clicar no botão
-document.querySelector('.tecla_kiko').play(); {
-}
+ //criando a referências constante listaDeTeclas e buscando a classe coletiva .tecla
+ const listaDeTeclas =document.querySelectorAll('.tecla');
  
+ let contador = 0;
+ while(contador < 9){
+   listaDeTeclas[contador].onclick = tocaSomPix;
+   contador = contador + 1;
+   console.log(contador);
+   
+}
