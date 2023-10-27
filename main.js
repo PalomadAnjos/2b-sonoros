cab27ec9a82073d7cbdda57c4aa7a8995346b7d3
paloma anjos <105317967+PalomadAnjos@users.noreply.github.com>
2023-10-27 10:55:36 -0300

    ok

--- main.js
+++ main.js
@@ -7,8 +7,10 @@ function tocaSomPix() {
  const listaDeTeclas =document.querySelectorAll('.tecla');
  
  let contador = 0;
- while(contador < 9){
-   listaDeTeclas[contador].onclick = function(){
+ while(contador < listaDeTeclas.length){
+   const efeito = listaDeTeclas[contador].classList[1]
+   const idAuio = "#som " + efeitos;
+ listaDeTeclas[contador].onclick = function(){
       tocaSom(idAuio);
    }
    contador = contador + 1;
