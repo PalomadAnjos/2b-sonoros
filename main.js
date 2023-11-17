39ffc0f021fe1ff7552136a39a13089f18a1fcc6
paloma anjos <105317967+PalomadAnjos@users.noreply.github.com>
2023-11-17 11:08:12 -0300

    ok

--- main.js
+++ main.js
@@ -5,9 +5,12 @@ function tocaSomPix() {
 //criando a referências constante listaDeTeclas e buscando a classe coletiva .tecla
  const listaDeTeclas =document.querySelectorAll('.tecla');
  //criação da repetição "while" trocado pelo "for" com variavel,condição e incremento
- for(let contador = 0; contador < istaDeTelas.
- 
- let contador = 0;
+ for(let contador = 0; contador < istaDeTeclas.length;contador++){
+  const teclas = listaDeTeclas[contador]
+  const efeito = 
+
+ }
+let contador = 0;
  while(contador < listaDeTeclas.length){
    const efeito = listaDeTeclas[contador].classList[1]
    const idAuio = "#som_" + efeito;
