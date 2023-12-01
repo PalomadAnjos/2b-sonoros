980d240008ecd23a959923381ad51da021374728
paloma anjos <105317967+PalomadAnjos@users.noreply.github.com>
2023-12-01 10:41:41 -0300

    ok

--- main.js
+++ main.js
@@ -1,22 +1,16 @@
 //Declaração/criação a função tocaSomPix
-function tocaSomPix() {
+function tocaSom() {
     document.querySelector(idElementoAudio).play(); 
  }
 //criando a referências constante listaDeTeclas e buscando a classe coletiva .tecla
  const listaDeTeclas =document.querySelectorAll('.tecla');
  //criação da repetição "while" trocado pelo "for" com variavel,condição e incremento
- for(let contador = 0; contador < istaDeTeclas.length;contador++){
-  const teclas = listaDeTeclas[contador]
-  const efeito = 
-
- }
-let contador = 0;
- while(contador < listaDeTeclas.length){
-   const efeito = listaDeTeclas[contador].classList[1]
-   const idAuio = "#som_" + efeito;
- listaDeTeclas[contador].onclick = function(){
-      tocaSom(idAuio);
+ for(let contador = 0; contador < listaDeTeclas.length;contador++){
+  const teclas = listaDeTeclas[contador];
+  const efeito = listaDeTeclas[contador].classList[1];
+   const idAudio = `#som_${efeito}`;
+ teclas.onclick = function(){
+      tocaSom(idAudio);
    }
-   contador = contador + 1;
-   console.log(contador);
+
    }
